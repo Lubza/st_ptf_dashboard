8459e497c91274ac595ee3c78551f60ae499a45f
Lubos <l.zahorak@gmail.com>
2025-07-01 17:10:28 +0200

    back

--- st_ptf_dashboard_app.py
+++ st_ptf_dashboard_app.py
@@ -20,15 +20,7 @@ if df.empty:
 else:
     st.subheader("Zoznam dividend")
     df['settleDate'] = pd.to_datetime(df['settleDate'], format='%Y%m%d').dt.strftime('%m/%d/%Y') # preformatuje datum
-    # Konverzia na datetime objekt
-    df['settleDate'] = pd.to_datetime(df['settleDate'], format='%Y%m%d')
 
-    # Extrakcia mesiaca (skratka) a roku
-    df['Month'] = df['settleDate'].dt.strftime('%b')  # Jan, Feb, ...
-    df['Year'] = df['settleDate'].dt.year
-
-    # (Nepovinné) Preformátovanie dátumu na string pre pekné zobrazenie
-    df['settleDate'] = df['settleDate'].dt.strftime('%m/%d/%Y')
     st.dataframe(df)
 
     st.subheader("Súhrn podľa meny")
