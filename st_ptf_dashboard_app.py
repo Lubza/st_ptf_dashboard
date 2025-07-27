9cd7aa362b220dc4e3fcf84799c4629c9d307d12
Lubos <l.zahorak@gmail.com>
2025-07-27 12:55:23 +0200

    resize and reconfigure divi page

--- st_ptf_dashboard_app.py
+++ st_ptf_dashboard_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 import altair as alt
 
+st.set_page_config(layout="wide")  # stránka bude širšia
+
 DB_URL = st.secrets["DB_URL"]
 TABLE_NAME = st.secrets["TABLE_NAME"]
 
@@ -18,86 +20,88 @@ df = load_data()
 if df.empty:
     st.warning("The dividends table is empty.")
 else:
-    # Pre istotu pretypuj dátum len raz
     df['settleDate'] = pd.to_datetime(df['settleDate'], format='%Y%m%d')
     df['Month'] = df['settleDate'].dt.strftime('%b')
     df['Year'] = df['settleDate'].dt.year
     df['settleDate_str'] = df['settleDate'].dt.strftime('%m/%d/%Y')
 
-    # Najskôr zoradíme podľa settleDate_str zostupne (najnovšie hore)
     df_sorted = df.sort_values("settleDate", ascending=False)
+    df_show = df_sorted[["symbol", "settleDate_str", "currency", "amount"]].reset_index(drop=True)
 
-    # Vyberieme len požadované stĺpce a v správnom poradí
-    df_show = df_sorted[["symbol", "settleDate_str", "currency", "amount"]]
-    df_show = df_show.reset_index(drop=True)
-    
-    # Zobrazíme tabuľku v streamlit
-    st.dataframe(df_show)
-   
-    tab1, tab2, tab3 = st.tabs(["Súhrn podľa roka", "Súhrn podľa mesiaca", "Súhrn podľa tickera"])
+    # --- Rozloženie do stĺpcov
+    col1, col2 = st.columns([1.3, 2.7])  # Pomer šírok
 
-    with tab1:
-        st.subheader("Summary by Year (in original currency)")
-        st.bar_chart(df.groupby("Year")["amount"].sum())
+    with col1:
+        st.dataframe(df_show, height=520)
 
-        summary = df.groupby(['Year', 'currency'])['amount'].sum().reset_index()
-        st.subheader("Summary by Year and Currency (Stacked Bar Chart)")
-        chart = alt.Chart(summary).mark_bar().encode(
-            x=alt.X('Year:O', title='Year'),
-            y=alt.Y('amount:Q', title='Sum of Dividends'),
-            color=alt.Color('currency:N', title='Currency'),
-            tooltip=['Year', 'currency', 'amount']
-        ).properties(width=600)
-        st.altair_chart(chart, use_container_width=True)
-
-    with tab2:
-        st.subheader("Mesačný prehľad podľa roka")
-
-        selected_year = st.selectbox('Vyber rok:', sorted(df['Year'].unique()), key="year_select")
-        df_year = df[df['Year'] == selected_year]
-        summary_month = (
-            df_year.groupby(['Month', 'currency'])['amount']
-            .sum()
-            .reset_index()
-        )
-        months_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        summary_month['Month'] = pd.Categorical(summary_month['Month'], categories=months_order, ordered=True)
-        summary_month = summary_month.sort_values('Month')
-        st.subheader(f"Summary by Month and Currency ({selected_year})")
-        chart1 = alt.Chart(summary_month).mark_bar().encode(
-            x=alt.X('Month:O', title='Month', sort=months_order),
-            y=alt.Y('amount:Q', title='Sum of Dividends'),
-            color=alt.Color('currency:N', title='Currency'),
-            tooltip=['Month', 'currency', 'amount']
-        ).properties(width=700)
-        st.altair_chart(chart1, use_container_width=True)
-
-        # Tab 3: Súhrn podľa tickera
-    with tab3:
-        st.subheader("Výber tickerov")
-        ticker_options = sorted(df['symbol'].dropna().unique())
-        selected_tickers = st.multiselect(
-            "Zvoľ jeden alebo viac tickerov:",
-            options=ticker_options,
-            default=ticker_options[:1],  # predvybraný prvý ticker
-            key="ticker_select"
+    with col2:
+        tab1, tab2, tab3 = st.tabs(
+            [
+                "Súhrn podľa roka", 
+                "Súhrn podľa mesiaca", 
+                "Súhrn podľa tickera"
+            ]
         )
 
-        if selected_tickers:
-            df_ticker = df[df['symbol'].isin(selected_tickers)]
-            summary_ticker = (
-                df_ticker.groupby(['Year', 'symbol'])['amount']
+        with tab1:
+            st.subheader("Summary by Year (in original currency)")
+            st.bar_chart(df.groupby("Year")["amount"].sum())
+            summary = df.groupby(['Year', 'currency'])['amount'].sum().reset_index()
+            st.subheader("Summary by Year and Currency (Stacked Bar Chart)")
+            chart = alt.Chart(summary).mark_bar().encode(
+                x=alt.X('Year:O', title='Year'),
+                y=alt.Y('amount:Q', title='Sum of Dividends'),
+                color=alt.Color('currency:N', title='Currency'),
+                tooltip=['Year', 'currency', 'amount']
+            ).properties(width=600)
+            st.altair_chart(chart, use_container_width=True)
+
+        with tab2:
+            st.subheader("Mesačný prehľad podľa roka")
+            selected_year = st.selectbox('Vyber rok:', sorted(df['Year'].unique()), key="year_select")
+            df_year = df[df['Year'] == selected_year]
+            summary_month = (
+                df_year.groupby(['Month', 'currency'])['amount']
                 .sum()
                 .reset_index()
             )
-            st.subheader(f"Súhrn dividend podľa tickera a roka")
-            chart2 = alt.Chart(summary_ticker).mark_bar().encode(
-                x=alt.X('Year:O', title='Year'),
+            months_order = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            summary_month['Month'] = pd.Categorical(summary_month['Month'], categories=months_order, ordered=True)
+            summary_month = summary_month.sort_values('Month')
+            st.subheader(f"Summary by Month and Currency ({selected_year})")
+            chart1 = alt.Chart(summary_month).mark_bar().encode(
+                x=alt.X('Month:O', title='Month', sort=months_order),
                 y=alt.Y('amount:Q', title='Sum of Dividends'),
-                color=alt.Color('symbol:N', title='Ticker'),
-                tooltip=['Year', 'symbol', 'amount']
+                color=alt.Color('currency:N', title='Currency'),
+                tooltip=['Month', 'currency', 'amount']
             ).properties(width=700)
-            st.altair_chart(chart2, use_container_width=True)
-        else:
-            st.info("Vyber aspoň jeden ticker na zobrazenie grafu.")
+            st.altair_chart(chart1, use_container_width=True)
+
+        with tab3:
+            st.subheader("Výber tickerov")
+            ticker_options = sorted(df['symbol'].dropna().unique())
+            selected_tickers = st.multiselect(
+                "Zvoľ jeden alebo viac tickerov:",
+                options=ticker_options,
+                default=ticker_options[:1],  # predvybraný prvý ticker
+                key="ticker_select"
+            )
+
+            if selected_tickers:
+                df_ticker = df[df['symbol'].isin(selected_tickers)]
+                summary_ticker = (
+                    df_ticker.groupby(['Year', 'symbol'])['amount']
+                    .sum()
+                    .reset_index()
+                )
+                st.subheader(f"Súhrn dividend podľa tickera a roka")
+                chart2 = alt.Chart(summary_ticker).mark_bar().encode(
+                    x=alt.X('Year:O', title='Year'),
+                    y=alt.Y('amount:Q', title='Sum of Dividends'),
+                    color=alt.Color('symbol:N', title='Ticker'),
+                    tooltip=['Year', 'symbol', 'amount']
+                ).properties(width=700)
+                st.altair_chart(chart2, use_container_width=True)
+            else:
+                st.info("Vyber aspoň jeden ticker na zobrazenie grafu.")
